ff8c9a7c071093212d936bf6ea566f19e36b9c00
Sergei <serg.dudk@gmail.com>
2018-02-08 17:44:42 +0100

    Move paths to main

--- cmd/phono/main.go
+++ cmd/phono/main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 
 	"github.com/dudk/phono"
 )
 
-var vstPaths []string
+var vstPaths = getVstScanPaths()
 
 var vst2 phono.Vst2
 
 func main() {
+
 	fmt.Printf("%v\n", vstPaths)
 	vst2 = phono.NewVst2(vstPaths)
-	//pool.New(vstPaths)
 
 	fmt.Println(vst2)
+}
+
+func getVstScanPaths() (paths []string) {
+	switch goos := runtime.GOOS; goos {
+	case "darwin":
+		paths = []string{
+			"~/Library/Audio/Plug-Ins/VST",
+			"/Library/Audio/Plug-Ins/VST",
+		}
+	case "windows":
+		paths = []string{
+			"C:\\Program Files (x86)\\Steinberg\\VSTPlugins",
+			"C:\\Program Files\\Steinberg\\VSTPlugins ",
+		}
+		envVstPath := os.Getenv("VST_PATH")
+		if len(envVstPath) > 0 {
+			paths = append(paths, envVstPath)
+		}
+	}
+	return
 }
