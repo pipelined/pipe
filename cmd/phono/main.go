f208a6f7b0b5c9d78c64bb3b33c84787d59bef83
Sergei <serg.dudk@gmail.com>
2018-02-09 14:56:23 +0100

    Add command processing

--- cmd/phono/main.go
+++ cmd/phono/main.go
@@ -1,41 +1,75 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"runtime"
-
-	"github.com/dudk/phono"
 )
 
-var vstPaths = getVstScanPaths()
-
-var vst2 phono.Vst2
-
-func main() {
-
-	fmt.Printf("%v\n", vstPaths)
-	vst2 = phono.NewVst2(vstPaths)
-
-	fmt.Println(vst2)
+type config struct {
+	args []string
 }
 
-func getVstScanPaths() (paths []string) {
-	switch goos := runtime.GOOS; goos {
-	case "darwin":
-		paths = []string{
-			"~/Library/Audio/Plug-Ins/VST",
-			"/Library/Audio/Plug-Ins/VST",
-		}
-	case "windows":
-		paths = []string{
-			"C:\\Program Files (x86)\\Steinberg\\VSTPlugins",
-			"C:\\Program Files\\Steinberg\\VSTPlugins ",
-		}
-		envVstPath := os.Getenv("VST_PATH")
-		if len(envVstPath) > 0 {
-			paths = append(paths, envVstPath)
+type command interface {
+	Name() string
+	Help() string
+	Run() error
+	Register(*flag.FlagSet)
+}
+
+func (config *config) run() int {
+	cmdName, args := parseArgs(config.args)
+	if cmdName == "" {
+		printUsage()
+		return errorExitCode
+	}
+
+	for _, cmd := range commands {
+		if cmd.Name() == cmdName {
+			flags := flag.NewFlagSet(cmdName, flag.ExitOnError)
+			cmd.Register(flags)
+			if err := flags.Parse(args); err != nil {
+				flags.PrintDefaults()
+				return errorExitCode
+			}
+			if err := cmd.Run(); err != nil {
+				fmt.Printf("Command failed: %v", err)
+				return errorExitCode
+			}
 		}
 	}
-	return
+
+	return successExitCode
+}
+
+var (
+	successExitCode = 0
+	errorExitCode   = 1
+	commands        []command
+)
+
+func main() {
+	commands = []command{&listCommand{}}
+	c := config{
+		args: os.Args,
+	}
+	os.Exit(c.run())
+}
+
+func parseArgs(args []string) (string, []string) {
+	if len(args) < 2 {
+		return "", nil
+	}
+	return args[1], args[2:]
+}
+
+func printUsage() {
+	fmt.Println("Phono is a CLI VST host")
+	fmt.Println()
+	fmt.Println("Usage: phono <command>")
+	fmt.Println()
+	fmt.Println("Commands:")
+	for _, cmd := range commands {
+		fmt.Printf("\t%s\t%s\n", cmd.Name(), cmd.Help())
+	}
 }
