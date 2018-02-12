bd6960d7f232d749df7c73e10162895f1f831c8c
Sergei <serg.dudk@gmail.com>
2018-02-12 15:38:05 +0100

    Remove source and add draft for wav sink

---
+++ audio/sink/sink_test.go
@@ -0,0 +1,35 @@
+package sink
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/dudk/phono/audio/pump"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWavSink(t *testing.T) {
+	pump := pump.Wav{
+		Path:       "../../_testdata/test.wav",
+		BufferSize: 512,
+	}
+	sink := Wav{
+		Path:       "../../_testdata/out.wav",
+		BufferSize: 512,
+		BitDepth:   16,
+		SampleRate: 44100,
+	}
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	out, _, err := pump.Pump(ctx)
+	assert.Nil(t, err)
+
+	errorc, err := sink.Sink(ctx, out)
+	assert.Nil(t, err)
+	for err = range errorc {
+		fmt.Printf("Error waiting for sink: %v", err)
+	}
+	assert.Nil(t, err)
+}
