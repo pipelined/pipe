d3033f3550fed8e46230891e7cb040515ef2a13d
Sergei <serg.dudk@gmail.com>
2018-02-13 16:36:22 +0100

    Add tests for cache

--- vst2/vst2_test.go
+++ vst2/vst2_test.go
@@ -1,1 +1,15 @@
 package vst2_test
+
+import (
+	"testing"
+
+	"github.com/dudk/phono/vst2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCache(t *testing.T) {
+	testPath := "../_testdata/vst2"
+	cache := vst2.NewCache([]string{testPath})
+	assert.NotNil(t, cache.Libs[testPath])
+	assert.Equal(t, 1, len(cache.Libs[testPath]))
+}
