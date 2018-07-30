58227a12600dc59ae6c353d79fc3f4db9138d312
dudk <serg.dudk@gmail.com>
2018-07-30 08:18:15 +0200

    Add asset package

---
+++ asset/asset_test.go
@@ -0,0 +1,86 @@
+package asset_test
+
+import (
+	"testing"
+
+	"github.com/dudk/phono/pipe/runner"
+
+	"github.com/dudk/phono"
+	"github.com/dudk/phono/asset"
+	"github.com/dudk/phono/mock"
+	"github.com/dudk/phono/pipe"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	tests = []struct {
+		phono.BufferSize
+		phono.NumChannels
+		mock.Limit
+		value    float64
+		messages int64
+		samples  int64
+	}{
+		{
+			NumChannels: 1,
+			Limit:       10,
+			value:       0.5,
+			messages:    10,
+			samples:     100,
+		},
+		{
+			NumChannels: 2,
+			Limit:       100,
+			value:       0.7,
+			messages:    100,
+			samples:     1000,
+		},
+	}
+	bufferSize = phono.BufferSize(10)
+)
+
+func TestPipe(t *testing.T) {
+	pump := &mock.Pump{
+		Limit:      1,
+		BufferSize: bufferSize,
+	}
+	processor := &mock.Processor{}
+
+	for _, test := range tests {
+		sink := &asset.Asset{}
+		p := pipe.New(
+			pipe.WithName("Mock"),
+			pipe.WithPump(pump),
+			pipe.WithProcessors(processor),
+			pipe.WithSinks(sink),
+		)
+		params := phono.NewParams(
+			pump.LimitParam(test.Limit),
+			pump.NumChannelsParam(test.NumChannels),
+			pump.ValueParam(test.value),
+		)
+		p.Push(params)
+		err := p.Do(pipe.Run)
+		assert.Nil(t, err)
+
+		messageCount, samplesCount := pump.Count()
+		assert.Equal(t, test.messages, messageCount)
+		assert.Equal(t, test.samples, samplesCount)
+
+		messageCount, samplesCount = processor.Count()
+		assert.Equal(t, test.messages, messageCount)
+		assert.Equal(t, test.samples, samplesCount)
+
+		assert.Equal(t, test.NumChannels, sink.Buffer.NumChannels())
+		assert.Equal(t, phono.BufferSize(test.samples), sink.Buffer.Size())
+
+		for i := range sink.Buffer {
+			for j := range sink.Buffer[i] {
+				assert.Equal(t, test.value, sink.Buffer[i][j])
+			}
+		}
+		err = p.Do(pipe.Run)
+		assert.NotNil(t, err)
+		assert.Equal(t, runner.ErrSingleUseReused, err)
+	}
+}
