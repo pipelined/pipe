2b4ff96c564466f98d7f000b58a99e7dc8a9fbcc
dudk <serg.dudk@gmail.com>
2018-09-09 00:31:04 +0200

    Fix examples for new article

--- example/example1.go
+++ example/example1.go
@@ -7,29 +7,34 @@ import (
 	"github.com/dudk/phono/wav"
 )
 
-// Example 1:
+// Example:
 //		Read .wav file
 //		Play it with portaudio
 func one() {
 	wavPath := "../_testdata/sample1.wav"
 	bufferSize := phono.BufferSize(512)
+	// wav pump
 	wavPump, err := wav.NewPump(
 		wavPath,
 		bufferSize,
 	)
 	check(err)
 
+	// portaudio sink
 	paSink := portaudio.NewSink(
 		bufferSize,
 		wavPump.WavSampleRate(),
 		wavPump.WavNumChannels(),
 	)
 
+	// build pipe
 	p := pipe.New(
 		pipe.WithPump(wavPump),
 		pipe.WithSinks(paSink),
 	)
 	defer p.Close()
+
+	// run pipe
 	err = p.Do(pipe.Run)
 	check(err)
 }
