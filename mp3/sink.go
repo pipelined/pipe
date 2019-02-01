d20283a83b1fe096b0ccd07fc2b94552ca34c97e
dudk <serg.dudk@gmail.com>
2019-02-01 22:56:09 +0100

    pipe/signal-prop: fix mp3 package

--- mp3/sink.go
+++ mp3/sink.go
@@ -6,35 +6,30 @@ import (
 	"os"
 	"sync"
 
+	"github.com/pipelined/phono/signal"
+
 	"github.com/pipelined/phono"
 	"github.com/viert/lame"
 )
 
 // Sink allows to send data to mp3 files.
 type Sink struct {
-	f    *os.File
-	wr   *lame.LameWriter
-	once sync.Once
+	path    string
+	bitRate int
+	quality int
+	f       *os.File
+	wr      *lame.LameWriter
+	once    sync.Once
 }
 
 // NewSink creates new Sink.
-func NewSink(path string, sampleRate int, numChannels int, bitRate int, quality int) (*Sink, error) {
-	f, err := os.Create(path)
-	if err != nil {
-		return nil, err
-	}
+func NewSink(path string, bitRate int, quality int) *Sink {
 	s := Sink{
-		f:  f,
-		wr: lame.NewWriter(f),
+		path:    path,
+		bitRate: bitRate,
+		quality: quality,
 	}
-	s.wr.Encoder.SetBitrate(bitRate)
-	s.wr.Encoder.SetQuality(quality)
-	s.wr.Encoder.SetNumChannels(int(numChannels))
-	s.wr.Encoder.SetInSamplerate(int(sampleRate))
-	s.wr.Encoder.SetMode(lame.JOINT_STEREO)
-	s.wr.Encoder.SetVBR(lame.VBR_RH)
-	s.wr.Encoder.InitParams()
-	return &s, nil
+	return &s
 }
 
 // Reset is used to prevent additional runs of the sink.
@@ -53,10 +48,25 @@ func (s *Sink) Flush(string) error {
 }
 
 // Sink writes buffer into file.
-func (s *Sink) Sink(string) (func(phono.Buffer) error, error) {
+func (s *Sink) Sink(sourceID string, sampleRate int, numChannels int) (func(phono.Buffer) error, error) {
+	var err error
+	s.f, err = os.Create(s.path)
+	if err != nil {
+		return nil, err
+	}
+
+	s.wr = lame.NewWriter(s.f)
+	s.wr.Encoder.SetBitrate(s.bitRate)
+	s.wr.Encoder.SetQuality(s.quality)
+	s.wr.Encoder.SetNumChannels(int(numChannels))
+	s.wr.Encoder.SetInSamplerate(int(sampleRate))
+	s.wr.Encoder.SetMode(lame.JOINT_STEREO)
+	s.wr.Encoder.SetVBR(lame.VBR_RH)
+	s.wr.Encoder.InitParams()
+
 	return func(b phono.Buffer) error {
 		buf := new(bytes.Buffer)
-		ints := b.Ints()
+		ints := signal.Float64(b).AsInterInt(signal.BitDepth16)
 		for i := range ints {
 			if err := binary.Write(buf, binary.LittleEndian, int16(ints[i])); err != nil {
 				return err
