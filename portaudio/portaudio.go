5c330da76adfd1f32d7226f9eca40b1f5f1a0fe8
dudk <serg.dudk@gmail.com>
2019-02-01 22:21:33 +0100

    pipe/signal-prop: fix portaudio package

--- portaudio/portaudio.go
+++ portaudio/portaudio.go
@@ -8,32 +8,28 @@ import (
 type (
 	// Sink represets portaudio sink which allows to play audio using default device.
 	Sink struct {
-		buf         []float32
-		stream      *portaudio.Stream
-		sampleRate  int
-		bufferSize  int
-		numChannels int
+		buf        []float32
+		stream     *portaudio.Stream
+		bufferSize int
 	}
 )
 
 // NewSink returns new initialized sink which allows to play pipe.
-func NewSink(bufferSize int, sampleRate int, numChannels int) *Sink {
+func NewSink(bufferSize int) *Sink {
 	return &Sink{
-		bufferSize:  bufferSize,
-		sampleRate:  sampleRate,
-		numChannels: numChannels,
+		bufferSize: bufferSize,
 	}
 }
 
 // Sink writes the buffer of data to portaudio stream.
 // It aslo initilizes a portaudio api with default stream.
-func (s *Sink) Sink(string) (func(phono.Buffer) error, error) {
-	s.buf = make([]float32, s.bufferSize*s.numChannels)
+func (s *Sink) Sink(sourceID string, sampleRate, numChannels int) (func(phono.Buffer) error, error) {
+	s.buf = make([]float32, s.bufferSize*numChannels)
 	err := portaudio.Initialize()
 	if err != nil {
 		return nil, err
 	}
-	s.stream, err = portaudio.OpenDefaultStream(0, s.numChannels, float64(s.sampleRate), s.bufferSize, &s.buf)
+	s.stream, err = portaudio.OpenDefaultStream(0, numChannels, float64(sampleRate), s.bufferSize, &s.buf)
 	if err != nil {
 		return nil, err
 	}
@@ -44,7 +40,7 @@ func (s *Sink) Sink(string) (func(phono.Buffer) error, error) {
 	return func(b phono.Buffer) error {
 		for i := range b[0] {
 			for j := range b {
-				s.buf[i*s.numChannels+j] = float32(b[j][i])
+				s.buf[i*numChannels+j] = float32(b[j][i])
 			}
 		}
 		return s.stream.Write()
