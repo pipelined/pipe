99389ebf6217ccd3cdb118571895a37288b2a141
dudk <serg.dudk@gmail.com>
2019-07-17 08:12:56 +0200

    new-runner: split runner into separate package

--- network.go
+++ network.go
@@ -1,7 +1,9 @@
 package pipe
 
 import (
+	"github.com/pipelined/pipe/internal/runner"
 	"github.com/pipelined/pipe/internal/state"
+	"github.com/pipelined/pipe/metric"
 )
 
 // Net controls the execution of pipes.
@@ -16,11 +18,11 @@ type Net struct {
 type chain struct {
 	uid        string
 	sampleRate int
-	pump       *pumpRunner
-	processors []*processRunner
-	sinks      []*sinkRunner
+	pump       *runner.Pump
+	processors []*runner.Processor
+	sinks      []*runner.Sink
 	components map[interface{}]string
-	takec      chan message // emission of messages
+	takec      chan runner.Message // emission of messages
 	params     state.Params
 }
 
@@ -57,41 +59,55 @@ func Network(ps ...*Pipe) (*Net, error) {
 
 func bindPipe(p *Pipe) (chain, error) {
 	components := make(map[interface{}]string)
-	uid := newUID()
-	// newPumpRunner should not be created here.
-	pumpRunner, sampleRate, numChannels, err := bindPump(uid, p.Pump)
+	pipeID := newUID()
+	// bind pump
+	pumpFn, sampleRate, numChannels, err := p.Pump.Pump(pipeID)
 	if err != nil {
 		return chain{}, err
 	}
+	pumpRunner := &runner.Pump{
+		Fn:    pumpFn,
+		Meter: metric.Meter(p.Pump, sampleRate),
+		Hooks: runner.BindHooks(p.Pump),
+	}
 	components[p.Pump] = newUID()
 
-	processorRunners := make([]*processRunner, 0, len(p.Processors))
-	for _, processor := range p.Processors {
-		r, err := bindProcessor(uid, sampleRate, numChannels, processor)
+	// bind processors
+	processorRunners := make([]*runner.Processor, 0, len(p.Processors))
+	for _, proc := range p.Processors {
+		processFn, err := proc.Process(pipeID, sampleRate, numChannels)
 		if err != nil {
 			return chain{}, err
 		}
-		processorRunners = append(processorRunners, r)
-		components[processor] = newUID()
+		processorRunners = append(processorRunners, &runner.Processor{
+			Fn:    processFn,
+			Meter: metric.Meter(proc, sampleRate),
+			Hooks: runner.BindHooks(proc),
+		})
+		components[proc] = newUID()
 	}
-	// create all runners
-	sinkRunners := make([]*sinkRunner, 0, len(p.Sinks))
+
+	// bind sinks
+	sinkRunners := make([]*runner.Sink, 0, len(p.Sinks))
 	for _, sink := range p.Sinks {
-		// sinkRunner should not be created here.
-		r, err := bindSink(uid, sampleRate, numChannels, sink)
+		sinkFn, err := sink.Sink(pipeID, sampleRate, numChannels)
 		if err != nil {
 			return chain{}, err
 		}
-		sinkRunners = append(sinkRunners, r)
+		sinkRunners = append(sinkRunners, &runner.Sink{
+			Fn:    sinkFn,
+			Meter: metric.Meter(sink, sampleRate),
+			Hooks: runner.BindHooks(sink),
+		})
 		components[sink] = newUID()
 	}
 	return chain{
-		uid:        uid,
+		uid:        pipeID,
 		sampleRate: sampleRate,
 		pump:       pumpRunner,
 		processors: processorRunners,
 		sinks:      sinkRunners,
-		takec:      make(chan message),
+		takec:      make(chan runner.Message),
 		components: components,
 		params:     make(map[string][]func()),
 	}, nil
@@ -115,14 +131,14 @@ func start(n *Net) state.StartFunc {
 		for _, c := range n.chains {
 			// start pump
 			componentID := c.components[c.pump]
-			out, errc := c.pump.run(bufferSize, c.uid, componentID, cancelc, provide, c.takec)
+			out, errc := c.pump.Run(bufferSize, c.uid, componentID, cancelc, provide, c.takec)
 			errcList = append(errcList, errc)
 
 			// start chained processesing
 			for _, proc := range c.processors {
 				componentID = c.components[proc]
 				// meter := n.metric.Meter(componentID, n.sampleRate)
-				out, errc = proc.run(c.uid, componentID, cancelc, out)
+				out, errc = proc.Run(c.uid, componentID, cancelc, out)
 				errcList = append(errcList, errc)
 			}
 
@@ -134,20 +150,20 @@ func start(n *Net) state.StartFunc {
 }
 
 // broadcastToSinks passes messages to all sinks.
-func broadcastToSinks(sampleRate int, c chain, cancelc chan struct{}, in <-chan message) []<-chan error {
+func broadcastToSinks(sampleRate int, c chain, cancelc chan struct{}, in <-chan runner.Message) []<-chan error {
 	//init errcList for sinks error channels
 	errcList := make([]<-chan error, 0, len(c.sinks))
 	//list of channels for broadcast
-	broadcasts := make([]chan message, len(c.sinks))
+	broadcasts := make([]chan runner.Message, len(c.sinks))
 	for i := range broadcasts {
-		broadcasts[i] = make(chan message)
+		broadcasts[i] = make(chan runner.Message)
 	}
 
 	//start broadcast
 	for i, s := range c.sinks {
 		componentID := c.components[s]
 		// meter := n.metric.Meter(componentID, n.sampleRate)
-		errc := s.run(c.uid, componentID, cancelc, broadcasts[i])
+		errc := s.Run(c.uid, componentID, cancelc, broadcasts[i])
 		errcList = append(errcList, errc)
 	}
 
@@ -160,10 +176,10 @@ func broadcastToSinks(sampleRate int, c chain, cancelc chan struct{}, in <-chan 
 		}()
 		for msg := range in {
 			for i := range broadcasts {
-				m := message{
-					sourceID: msg.sourceID,
-					buffer:   msg.buffer,
-					params:   msg.params.Detach(c.components[c.sinks[i]]),
+				m := runner.Message{
+					SourceID: msg.SourceID,
+					Buffer:   msg.Buffer,
+					Params:   msg.Params.Detach(c.components[c.sinks[i]]),
 				}
 				select {
 				case broadcasts[i] <- m:
@@ -182,9 +198,9 @@ func broadcastToSinks(sampleRate int, c chain, cancelc chan struct{}, in <-chan 
 func newMessage(n *Net) state.NewMessageFunc {
 	return func(pipeID string) {
 		c := n.chains[pipeID]
-		m := message{sourceID: c.uid}
+		m := runner.Message{SourceID: c.uid}
 		if len(c.params) > 0 {
-			m.params = c.params
+			m.Params = c.params
 			c.params = make(map[string][]func())
 		}
 		c.takec <- m
