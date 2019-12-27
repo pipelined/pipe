4919a3b536bbd615dc8e8fcd57a793bcd5726781
Sergei Dudka <dudk@uber.com>
2019-12-27 22:32:27 +0100

    merge pipe and line files

--- pipe.go
+++ pipe.go
@@ -12,12 +12,88 @@ import (
 	"pipelined.dev/signal/pool"
 )
 
+// pipeline components
+type (
+	PumpFunc func() (Pump, signal.SampleRate, int, error)
+	// Pump is a source of samples. Pump method returns a new buffer with signal data.
+	// If no data is available, io.EOF should be returned. If pump cannot provide data
+	// to fulfill buffer, it can trim the size of the buffer to align it with actual data.
+	// Buffer size can only be decreased.
+	Pump struct {
+		Pump func(out signal.Float64) error
+		Hooks
+	}
+
+	ProcessorFunc func(signal.SampleRate, int) (Processor, signal.SampleRate, int, error)
+	// Processor defines interface for pipe processors.
+	// Processor should return output in the same signal buffer as input.
+	// It is encouraged to implement in-place processing algorithms.
+	// Buffer size could be changed during execution, but only decrease allowed.
+	// Number of channels cannot be changed.
+	Processor struct {
+		Process func(in, out signal.Float64) error
+		Hooks
+	}
+
+	SinkFunc func(signal.SampleRate, int) (Sink, error)
+	// Sink is an interface for final stage in audio pipeline.
+	// This components must not change buffer content. Line can have
+	// multiple sinks and this will cause race condition.
+	Sink struct {
+		Sink func(in signal.Float64) error
+		Hooks
+	}
+
+	Hook  func() error
+	Hooks struct {
+		Flush     Hook
+		Interrupt Hook
+		Reset     Hook
+	}
+)
+
+// optional interfaces
+type (
+	// Resetter is a component that must be resetted before new run.
+	// Reset hook is executed when Run happens.
+	Resetter interface {
+		Reset() error
+	}
+
+	// Interrupter is a component that has custom interruption logic.
+	// Interrupt hook is executed when Cancel happens.
+	Interrupter interface {
+		Interrupt() error
+	}
+
+	// Flusher is a component that must be flushed in the end of execution.
+	// Flush hook is executed in the end of the run. It will be skipped if Reset hook has failed.
+	Flusher interface {
+		Flush() error
+	}
+)
+
+// Line is a sound processing sequence of components.
+// It has a single pump, zero or many processors executed sequentially
+// and one or many sinks executed in parallel.
+type Line struct {
+	Pump       PumpFunc
+	Processors []ProcessorFunc
+	Sinks      []SinkFunc
+
+	numChannels int //TODO: replace with runners data
+	params      chan runner.Params
+	pump        runner.Pump
+	processors  []runner.Processor
+	sinks       []runner.Sink
+}
+
 // Pipe controls the execution of multiple chained lines. Lines might be chained
 // through components, mixer for example.  If lines are not chained, they must be
 // controlled by separate Pipes. Use New constructor to instantiate new Pipes.
 type Pipe struct {
-	h     *state.Handle
-	lines map[chan runner.Params]*Line // map chain id to chain
+	handle *state.Handle
+	lines  map[chan runner.Params]*Line // map chain id to chain
 }
 
 // New creates a new pipeline.
@@ -30,15 +106,15 @@ func New(ls ...*Line) (*Pipe, error) {
 		if err != nil {
 			return nil, fmt.Errorf("error binding line: %w", err)
 		}
-		// map chains
+		// map lines
 		lines[l.params] = l
 	}
 
 	p := &Pipe{
 		lines: lines,
 	}
-	p.h = state.NewHandle(start(p))
-	go state.Loop(p.h)
+	p.handle = state.NewHandle(start(p))
+	go state.Loop(p.handle)
 	return p, nil
 }
 
@@ -119,31 +195,84 @@ func start(p *Pipe) state.StartFunc {
 // Calling this method after handle is closed causes a panic.
 // Feedback channel is closed when Ready state is reached or context is cancelled.
 func (p *Pipe) Run(ctx context.Context, bufferSize int) chan error {
-	return p.h.Run(ctx, bufferSize)
+	return p.handle.Run(ctx, bufferSize)
 }
 
 // Pause sends a pause event into handle.
 // Calling this method after handle is closed causes a panic.
 // Feedback is closed when Paused state is reached.
 func (p *Pipe) Pause() chan error {
-	return p.h.Pause()
+	return p.handle.Pause()
 }
 
 // Resume sends a resume event into handle.
 // Calling this method after handle is closed causes a panic.
 // Feedback is closed when Ready state is reached.
 func (p *Pipe) Resume() chan error {
-	return p.h.Resume()
+	return p.handle.Resume()
 }
 
 // Close must be called to clean up handle's resources.
 // Feedback is closed when line is done.
 func (p *Pipe) Close() chan error {
-	return p.h.Interrupt()
+	return p.handle.Interrupt()
 }
 
 // Push new params into pipe.
 // Calling this method after pipe is closed causes a panic.
 func (p *Pipe) Push(id string, paramFuncs ...func()) {
-	p.h.Push(runner.Params{id: paramFuncs})
+	p.handle.Push(runner.Params{id: paramFuncs})
+}
+
+// Processors is a helper function to use in line constructors.
+func Processors(processors ...ProcessorFunc) []ProcessorFunc {
+	return processors
+}
+
+// Sinks is a helper function to use in line constructors.
+func Sinks(sinks ...SinkFunc) []SinkFunc {
+	return sinks
+}
+
+// Wait for state transition or first error to occur.
+func Wait(d <-chan error) error {
+	for err := range d {
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// BindHooks of component.
+func BindHooks(v interface{}) runner.Hooks {
+	return runner.Hooks{
+		Flush:     flusher(v),
+		Interrupt: interrupter(v),
+		Reset:     resetter(v),
+	}
+}
+
+// flusher checks if interface implements Flusher and if so, return it.
+func flusher(i interface{}) runner.Hook {
+	if v, ok := i.(Flusher); ok {
+		return v.Flush
+	}
+	return nil
+}
+
+// flusher checks if interface implements Flusher and if so, return it.
+func interrupter(i interface{}) runner.Hook {
+	if v, ok := i.(Interrupter); ok {
+		return v.Interrupt
+	}
+	return nil
+}
+
+// flusher checks if interface implements Flusher and if so, return it.
+func resetter(i interface{}) runner.Hook {
+	if v, ok := i.(Resetter); ok {
+		return v.Reset
+	}
+	return nil
 }
