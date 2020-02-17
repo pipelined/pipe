4555869a8f86dc63aa4838e78df7e81f3cb5a33a
Sergei Dudka <serg.dudk@gmail.com>
2020-02-17 07:24:55 +0100

    rename splitter to repeater

--- repeat/repeat.go
+++ repeat/repeat.go
@@ -1,4 +1,4 @@
-package split
+package repeat
 
 import (
 	"context"
@@ -10,7 +10,7 @@ import (
 	"pipelined.dev/signal/pool"
 )
 
-type Splitter struct {
+type Repeater struct {
 	sampleRate  signal.SampleRate
 	numChannels int
 	pumps       []chan message
@@ -23,26 +23,26 @@ type message struct {
 }
 
 // Sink must be called once per broadcast.
-func (s *Splitter) Sink() pipe.SinkFunc {
+func (r *Repeater) Sink() pipe.SinkFunc {
 	return func(bufferSize int, sr signal.SampleRate, nc int) (pipe.Sink, error) {
-		s.sampleRate = sr
-		s.numChannels = nc
-		s.pool = pool.New(s.numChannels, bufferSize)
+		r.sampleRate = sr
+		r.numChannels = nc
+		r.pool = pool.New(r.numChannels, bufferSize)
 		var buffer *signal.Float64
 		return pipe.Sink{
 			Sink: func(b signal.Float64) error {
-				buffer = s.pool.Alloc()
+				buffer = r.pool.Alloc()
 				copyFloat64(*buffer, b)
-				for _, pump := range s.pumps {
+				for _, pump := range r.pumps {
 					pump <- message{
-						pumps:  int32(len(s.pumps)),
+						pumps:  int32(len(r.pumps)),
 						buffer: buffer,
 					}
 				}
 				return nil
 			},
 			Flush: func(context.Context) error {
-				for _, pump := range s.pumps {
+				for _, pump := range r.pumps {
 					close(pump)
 				}
 				return nil
@@ -52,9 +52,9 @@ func (s *Splitter) Sink() pipe.SinkFunc {
 }
 
 // Pump must be called at least once per broadcast.
-func (s *Splitter) Pump() pipe.PumpFunc {
+func (r *Repeater) Pump() pipe.PumpFunc {
 	pump := make(chan message, 1)
-	s.pumps = append(s.pumps, pump)
+	r.pumps = append(r.pumps, pump)
 	return func(bufferSize int) (pipe.Pump, signal.SampleRate, int, error) {
 		var (
 			message message
@@ -68,16 +68,16 @@ func (s *Splitter) Pump() pipe.PumpFunc {
 				}
 				copyFloat64(b, *message.buffer)
 				if atomic.AddInt32(&message.pumps, -1) == 0 {
-					s.pool.Free(message.buffer)
+					r.pool.Free(message.buffer)
 				}
 				return nil
 			},
-		}, s.sampleRate, s.numChannels, nil
+		}, r.sampleRate, r.numChannels, nil
 	}
 }
 
-func copyFloat64(d signal.Float64, s signal.Float64) {
-	for i := range s {
-		copy(d[i], s[i])
+func copyFloat64(d signal.Float64, r signal.Float64) {
+	for i := range r {
+		copy(d[i], r[i])
 	}
 }
