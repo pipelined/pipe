6df1dc34d432c13597182a18f1b0da5445b34ffa
Sergei Dudka <dudk@uber.com>
2020-02-24 07:32:15 +0100

    add repeater on the fly

---
+++ repeat/repeat_test.go
@@ -0,0 +1,49 @@
+package repeat_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"pipelined.dev/pipe"
+	"pipelined.dev/pipe/internal/mock"
+	"pipelined.dev/pipe/repeat"
+)
+
+const bufferSize = 512
+
+func TestAddRoute(t *testing.T) {
+	repeater := &repeat.Repeater{}
+	source, _ := pipe.Line{
+		Pump: (&mock.Pump{
+			Limit:       10 * bufferSize,
+			NumChannels: 2,
+		}).Pump(),
+		Sink: repeater.Sink(),
+	}.Route(bufferSize)
+
+	sink1 := &mock.Sink{}
+	destination1, _ := pipe.Line{
+		Pump: repeater.Pump(),
+		Sink: sink1.Sink(),
+	}.Route(bufferSize)
+
+	p := pipe.New(
+		context.Background(),
+		pipe.WithRoutes(source, destination1),
+	)
+	sink2 := &mock.Sink{}
+	line := pipe.Line{
+		Sink: sink2.Sink(),
+	}
+	repeaterHandler := source.Sink()
+	m := repeaterHandler.Mutate(repeater.AddLine(p, line))
+	p.Push(m)
+
+	// start
+	_ = p.Wait()
+	assert.Equal(t, 10, sink1.Counter.Messages)
+	assert.Equal(t, 10*bufferSize, sink1.Counter.Samples)
+	assert.True(t, sink2.Counter.Messages > 0)
+	assert.True(t, sink2.Counter.Samples > 0)
+}
