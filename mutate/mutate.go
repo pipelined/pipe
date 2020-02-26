5d0afda1ae8d8f3b5cb1487196b2f014d784c650
Sergei Dudka <dudk@uber.com>
2020-02-26 17:50:41 +0100

    make mutation contain single mutator

--- mutate/mutate.go
+++ mutate/mutate.go
@@ -11,21 +11,18 @@ type (
 	// Mutation is a set of mutators attached to a specific component.
 	Mutation struct {
 		Mutability
-		Mutators []Mutator
-		Puller   chan Mutators
+		Mutator func() error
+		Puller  chan Mutators
 	}
 )
 
-// Mutators is a set of mutators mapped to Receiver of their receivers.
-// TODO: maybe this should not be exported
-type Mutators map[[16]byte][]Mutator
-
-type Mutator func() error
-
-func (m Mutability) Mutate(ms ...Mutator) Mutation {
+func (m Mutability) Mutate(mutator func() error) Mutation {
+	if m == immutable {
+		return Mutation{}
+	}
 	return Mutation{
 		Mutability: m,
-		Mutators:   ms,
+		Mutator:    mutator,
 	}
 }
 
@@ -42,14 +39,11 @@ func Mutable() Mutability {
 	return id
 }
 
-// Receiver allows to identify the component mutator belongs to.
-// type Receiver [16]byte
+// Mutators is a set of mutators mapped to Receiver of their receivers.
+// TODO: maybe this should not be exported
+type Mutators map[[16]byte][]Mutator
 
-// func NewReceiver() Receiver {
-// 	var id [16]byte
-// 	rand.Read(id[:])
-// 	return id
-// }
+type Mutator func() error
 
 // Add appends a slice of Mutators.
 func (m Mutators) Add(id [16]byte, fns ...Mutator) Mutators {
