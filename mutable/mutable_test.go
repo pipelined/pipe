83a110509bdf87c27e82a4cac8ad3b97a91d67dd
Sergei Dudka <serg.dudk@gmail.com>
2020-06-03 20:50:53 +0200

    remove assert from mutable

--- mutable/mutable_test.go
+++ mutable/mutable_test.go
@@ -1,11 +1,10 @@
 package mutable_test
 
 import (
+	"reflect"
 	"testing"
 
 	"pipelined.dev/pipe/mutable"
-
-	"github.com/stretchr/testify/assert"
 )
 
 // paramMock used to set up test cases for mutators
@@ -84,7 +83,7 @@ func TestAddParams(t *testing.T) {
 		}
 		for _, m := range c.mocks {
 			mutators.ApplyTo(m.Mutable)
-			assert.Equal(t, m.expected, m.value)
+			assertEqual(t, "value", m.value, m.expected)
 		}
 	}
 }
@@ -135,7 +134,7 @@ func TestAppendParams(t *testing.T) {
 		}
 		for _, m := range c.mocks {
 			mutators.ApplyTo(m.Mutable)
-			assert.Equal(t, m.expected, m.value)
+			assertEqual(t, "value", m.value, m.expected)
 		}
 	}
 }
@@ -201,9 +200,16 @@ func TestDetachParams(t *testing.T) {
 		for _, m := range c.mocks {
 			d := mutators.Detach(m.Mutable)
 			mutators.ApplyTo(m.Mutable)
-			assert.Equal(t, 0, m.value)
+			assertEqual(t, "value before", m.value, 0)
 			d.ApplyTo(m.Mutable)
-			assert.Equal(t, m.expected, m.value)
+			assertEqual(t, "value after", m.value, m.expected)
 		}
+	}
+}
+
+func assertEqual(t *testing.T, name string, result, expected interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf("%v\nresult: \t%T\t%+v \nexpected: \t%T\t%+v", name, result, result, expected, expected)
 	}
 }
