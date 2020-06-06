d30f1c73956434e503e662b0649cd899fd2238d4
Sergei Dudka <serg.dudk@gmail.com>
2020-06-06 21:00:20 +0200

    add godocs

--- options.go
+++ options.go
@@ -2,8 +2,10 @@ package pipe
 
 import "pipelined.dev/pipe/mutability"
 
+// Option represents pipe constructor parameter.
 type Option func(*Pipe)
 
+// WithLines provides lines for the pipe.
 func WithLines(lines ...Line) Option {
 	return func(p *Pipe) {
 		for _, l := range lines {
@@ -12,11 +14,13 @@ func WithLines(lines ...Line) Option {
 	}
 }
 
+// WithMutations provides mutations for the pipe. The mutations will be
+// sent along the pipe with the first message.
 func WithMutations(mutations ...mutability.Mutation) Option {
 	return func(p *Pipe) {
 		for _, m := range mutations {
 			if c := p.listeners[m.Mutability]; c != nil {
-				p.mutatorsByListeners[c] = p.mutatorsByListeners[c].Put(m)
+				p.mutations[c] = p.mutations[c].Put(m)
 			}
 		}
 	}
