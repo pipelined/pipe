5946160f517f08cc3d6d10a32de8c7893cb1ced0
Sergei Dudka <serg.dudk@gmail.com>
2021-02-04 21:37:25 +0100

    move run to pipe internal

---
+++ internal/run/run.go
@@ -0,0 +1,45 @@
+package run
+
+import (
+	"context"
+	"fmt"
+	"io"
+)
+
+type (
+	// Executor executes a single DSP operation.
+	Executor interface {
+		Execute(context.Context) error
+		Start(context.Context) error
+		Flush(context.Context) error
+	}
+)
+
+// Run the component runner.
+func Run(ctx context.Context, e Executor) <-chan error {
+	errc := make(chan error, 1)
+	go run(ctx, e, errc)
+	return errc
+}
+
+func run(ctx context.Context, e Executor, errc chan<- error) {
+	defer close(errc)
+	if err := e.Start(ctx); err != nil {
+		errc <- fmt.Errorf("error starting component: %w", err)
+		return
+	}
+	defer func() {
+		if err := e.Flush(ctx); err != nil {
+			errc <- fmt.Errorf("error flushing component: %w", err)
+		}
+	}()
+
+	var err error
+	for err == nil {
+		err = e.Execute(ctx)
+	}
+	if err != io.EOF {
+		errc <- fmt.Errorf("error running component: %w", err)
+	}
+	return
+}
